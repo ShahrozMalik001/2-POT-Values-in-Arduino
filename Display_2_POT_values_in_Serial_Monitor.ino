2d98b0890d4bc3c4965785e2a06c28ddfbd5a695
Shahroz Malik <68744693+ShahrozMalik001@users.noreply.github.com>
2020-10-02 17:37:08 +0500

    I declared the variables outside of loop function
    Declaration of variables outside from the loop function to access it any function

--- Display_2_POT_values_in_Serial_Monitor.ino
+++ Display_2_POT_values_in_Serial_Monitor.ino
@@ -1,10 +1,12 @@
+int sensorValue1;
+int sensorValue2;
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue1 = analogRead(A0); 
-  int sensorValue2 = analogRead(A1);
+  sensorValue1 = analogRead(A0); 
+  sensorValue2 = analogRead(A1);
   Serial.print("POT 1 Val is : "); 
   Serial.print(sensorValue1);
   Serial.print(" & POT 2 Val is : ");
